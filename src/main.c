722af6e90e032e515f4857a2a318cd831cebfa69
BuildTools <janek2510@hotmail.com>
2023-11-12 23:20:09 +0100

    tensometer working

--- src/main.c
+++ src/main.c
@@ -1,3 +1,69 @@
+#include <stdio.h>
+#include <esp_err.h>
+#include <inttypes.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 
-void app_main() {}
+#include "HX711/hx711_lib.h"
+
+#define SCALE_CONST 233.82
+#define SCALE_AVERAGE_READS 10
+
+
+hx711_t tensometer;
+int32_t scale_offset = 0;
+
+int32_t tensometer_read_once(){
+    int32_t tensometer_data;    
+    hx711_read_data(&tensometer, &tensometer_data);
+    hx711_wait(&tensometer, 500);
+
+    return tensometer_data;
+}
+
+int32_t tensometer_read_average(){
+    int32_t tensometer_data = 0;
+    int32_t sum = 0;
+    int32_t mean = 0;
+    for (uint8_t i = 0; i < SCALE_AVERAGE_READS; i++){
+        hx711_read_data(&tensometer, &tensometer_data);
+        hx711_wait(&tensometer, 500);
+        sum += tensometer_data;
+    }
+    mean = sum/SCALE_AVERAGE_READS;
+    mean = mean - scale_offset;
+    mean = mean/SCALE_CONST;
+    return mean;
+
+}
+
+esp_err_t tensometer_init(){
+
+    tensometer.dout = 33;
+    tensometer.pd_sck = 32;
+    tensometer.gain = 0;
+
+    if(hx711_init(&tensometer) != ESP_OK){
+        printf("failed to initialise tensometer");
+        return ESP_ERR_INVALID_RESPONSE;
+    }
+
+    scale_offset = tensometer_read_once();
+
+    return ESP_OK;
+}
+
+
+
+void app_main() {
+
+    tensometer_init();
+
+    while(1){
+
+        printf("tensometer data: %" PRIi32 "\n", tensometer_read_average());
+        //printf("tensometer_raw data: %" PRIi32 "\n", tensometer_read_once());
+
+    }
+}
